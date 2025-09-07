8280f5bbc1192d80bab0f6fff64c9e9728e0830c
ToyBitZ00 <pa6308191@gmail.com>
2025-09-07 15:03:03 +0800

    Made 4 different Car objects from the Car Class and specified different kinds of Cars

---
+++ Test_Car.java
@@ -0,0 +1,9 @@
+public class Test_Car {
+    public static void main(String[] args) {
+        Car car1 = new Car("M3", "BMW", 2025, 59999.0);
+        Car car2 = new Car("Civic", "Honda", 2022, 39999.0);
+        Car car3 = new Car("Vios", "Toyota", 2024, 49000.0);
+        Car car4 = new Car("Innova", "Toyota", 2025, 59999.0);
+
+    }
+}
